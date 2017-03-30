aa909bafff444e35439fde9fb419b399fa0234eb
Gogis0 <adriangoga0@gmail.com>
2017-03-30 23:23:03 +0200

    Make it actually work :D

--- my_cat.c
+++ my_cat.c
@@ -10,39 +10,42 @@ pthread_cond_t mam_zapisane, mam_precitane;
 
 char buffer[bufsiz];
 int pocet = 0;
+int turn = 0;
 
 void *citaj(void *data) {
     while (1) {
         pthread_mutex_lock(&mutex);
-        printf("reader\n");
+        if (turn == 1) pthread_cond_wait(&mam_zapisane, &mutex);
+        //printf("reader\n");
         pocet = read(0, buffer, bufsiz);
-        pthread_cond_signal(&mam_precitane);
     
         if (pocet == 0) {
             pocet = -1;
-            puts("KONEEEEEEEEEEEEEEEEEEC");
+            //puts("KONEEEEEEEEEEEEEEEEEEC");
+            turn = 1;
+            pthread_cond_signal(&mam_precitane);
             pthread_mutex_unlock(&mutex);
             pthread_exit(NULL);
-        } else {
-            printf("mam precitane, idem spat\n");
-            pthread_cond_wait(&mam_zapisane, &mutex);
         }
+        turn = 1;
+        pthread_cond_signal(&mam_precitane);
+        pthread_mutex_unlock(&mutex);
     }
 }
 
 void *pis(void *data) {
     while (1) {
         pthread_mutex_lock(&mutex);
-        printf("writer\n");
+        //printf("writer\n");
+        if (turn == 0) pthread_cond_wait(&mam_precitane, &mutex);
+
         if (pocet == -1) {
             pthread_exit(NULL);
         }
-
-        //write(1, buffer, pocet);
+        write(1, buffer, pocet);
+        turn = 0;
         pthread_cond_signal(&mam_zapisane);
-        printf("dal som signal, ze mam zapisane\n");
-        pthread_cond_wait(&mam_precitane, &mutex);
-        printf("precooo sa tu nic nedeje\n");
+        pthread_mutex_unlock(&mutex);
     }
 }
 
@@ -50,15 +53,12 @@ int main() {
     pthread_t reader, writer;
     pthread_create(&reader, NULL, citaj, NULL);
     pthread_create(&writer, NULL, pis, NULL);
-    /*
+    
     (void) pthread_join(reader, NULL);
     (void) pthread_join(writer, NULL);
-    puts("return 0l");
+
     pthread_mutex_destroy(&mutex);
     pthread_cond_destroy(&mam_zapisane);
     pthread_cond_destroy(&mam_precitane);
     pthread_exit(NULL);
-    */
-    sleep(4);
-    return 0;
 }
