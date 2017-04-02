b8f2c31923cd8c0c1f04b996100538f510159f16
Gogis0 <adriangoga0@gmail.com>
2017-04-02 02:49:49 +0200

    Ultrafast memory allocashooons

--- fast_cat.c
+++ fast_cat.c
@@ -1,15 +1,26 @@
-#include "unistd.h"
 #include "stdio.h"
+#include <unistd.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 
 #define bufsiz (128 * 1024)
 
-char buffer[bufsiz];
+
+char *buffer;
 
 int main() {
+    int page_size = sysconf(_SC_PAGESIZE);
+    struct stat fs;
+    stat(0, &fs);
+    
+    int insize = bufsiz;
+    if (fs.st_blksize > insize) insize = fs.st_blksize;
+
+
+    buffer = malloc(insize+ page_size - 1);
     int pocet;
     do {
         pocet = read(0, buffer, bufsiz);
         write(1, buffer, pocet);
-    } while(pocet > 0);
+    } while (pocet > 0);
 }
