8c6310ab1e32c6dbbe2bef09a840427312c60b40
Gogis0 <adriangoga0@gmail.com>
2017-04-02 13:26:30 +0200

    Version 2.0 ... maybe

--- fast_cat.c
+++ fast_cat.c
@@ -3,24 +3,15 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
-#define bufsiz (128 * 1024)
-
-
 char *buffer;
 
 int main() {
     int page_size = sysconf(_SC_PAGESIZE);
-    struct stat fs;
-    stat(0, &fs);
-    
-    int insize = bufsiz;
-    if (fs.st_blksize > insize) insize = fs.st_blksize;
-
-
-    buffer = malloc(insize+ page_size - 1);
-    int pocet;
+        
+    int pocet = 0;
+    buffer = malloc(page_size * 32);
     do {
-        pocet = read(0, buffer, bufsiz);
+        pocet = read(0, buffer, page_size * 32);
         write(1, buffer, pocet);
     } while (pocet > 0);
 }
--- my_cat.c
+++ my_cat.c
@@ -1,34 +1,33 @@
 #include "unistd.h"
 #include "stdio.h"
 #include <sys/stat.h>
+#include <stdlib.h>
 #include <pthread.h>
 
-#define bufsiz (1 * 1024 * 1024)
-
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t mam_zapisane, mam_precitane;
+pthread_cond_t cond;
 
-char buffer[bufsiz];
+const int bufsiz_const = 32;
+int bufsiz;
+char *buffer;
 int pocet = 0;
 int turn = 0;
 
 void *citaj(void *data) {
     while (1) {
         pthread_mutex_lock(&mutex);
-        if (turn == 1) pthread_cond_wait(&mam_zapisane, &mutex);
-        //printf("reader\n");
+        if (turn == 1) pthread_cond_wait(&cond, &mutex);
         pocet = read(0, buffer, bufsiz);
     
         if (pocet == 0) {
             pocet = -1;
-            //puts("KONEEEEEEEEEEEEEEEEEEC");
             turn = 1;
-            pthread_cond_signal(&mam_precitane);
+            pthread_cond_signal(&cond);
             pthread_mutex_unlock(&mutex);
             pthread_exit(NULL);
         }
         turn = 1;
-        pthread_cond_signal(&mam_precitane);
+        pthread_cond_signal(&cond);
         pthread_mutex_unlock(&mutex);
     }
 }
@@ -36,20 +35,23 @@ void *citaj(void *data) {
 void *pis(void *data) {
     while (1) {
         pthread_mutex_lock(&mutex);
-        //printf("writer\n");
-        if (turn == 0) pthread_cond_wait(&mam_precitane, &mutex);
+        if (turn == 0) pthread_cond_wait(&cond, &mutex);
 
         if (pocet == -1) {
             pthread_exit(NULL);
         }
         write(1, buffer, pocet);
         turn = 0;
-        pthread_cond_signal(&mam_zapisane);
+        pthread_cond_signal(&cond);
         pthread_mutex_unlock(&mutex);
     }
 }
 
 int main() {
+    int page_size =  sysconf(_SC_PAGESIZE); 
+    bufsiz = page_size * bufsiz_const;
+    buffer = malloc(bufsiz);
+
     pthread_t reader, writer;
     pthread_create(&reader, NULL, citaj, NULL);
     pthread_create(&writer, NULL, pis, NULL);
@@ -58,7 +60,6 @@ int main() {
     (void) pthread_join(writer, NULL);
 
     pthread_mutex_destroy(&mutex);
-    pthread_cond_destroy(&mam_zapisane);
-    pthread_cond_destroy(&mam_precitane);
+    pthread_cond_destroy(&cond);
     pthread_exit(NULL);
 }
